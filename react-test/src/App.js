9bceb329c6ff664c00a780b0f74fc55aaa7a50aa
pro <60010427@kmitl.ac.th>
2021-02-24 16:20:28 +0700

    update app
    change from class to function

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,47 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
-import React,{ Component ,useState } from 'react';
+import React,{ useState } from 'react';
 
-class App extends Component {
-  state = {
-    selectedFile: null
-  }
-  data = []
-
-
-  buttonClick = () => {
+const App = () => {
+  const [state,setState] = useState({selectedFile : null});
+  const [data,setData] = useState({selectedFile:null , filename:null});
+  
+  const buttonClick = () => {
   console.log("click");
   axios.post("http://127.0.0.1:8000/test?name=gfd&gender=dfg").then(res => {console.log(res)});
   }
 
-  fileSelectedHandler = event => {
+  const  fileSelectedHandler = event => {
     console.log(event.target.files[0]);
-    this.setState({selectedFile: event.target.files[0]})
+    setState({selectedFile: event.target.files[0]})
+    setData({selectedFile: URL.createObjectURL(event.target.files[0]),filename: event.target.files[0].name})
+    // console.log(state.data.filename)
   }
 
-  fileUploadHandler = () => {
+  const  fileUploadHandler = () => {
     const fd = new FormData();
-    fd.append("image",this.state.selectedFile,this.state.selectedFile.name);
-    axios.post("http://127.0.0.1:8000/detect",fd).then(res => {console.log(res.data)});
+    fd.append("image",state.selectedFile,state.selectedFile.name);
+    axios.post("http://127.0.0.1:8000/detect",fd).then(res => {
+      console.log(res.data);
+    });
   }
 
-  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{this.data}</p>
-        <button onClick={this.buttonClick}>test API</button>
-        <input type="file" onChange={this.fileSelectedHandler}></input>
-        <button onClick={this.fileUploadHandler}>test upload</button>
+        <img src={data.selectedFile}/>
+        <p>{data.filename}</p>
+        <button onClick={buttonClick}>test API</button>
+        <input type="file" onChange={fileSelectedHandler}></input>
+        <button onClick={fileUploadHandler}>test upload</button>
       </header>
     </div>
   );
-  }
 }
 
 export default App;
